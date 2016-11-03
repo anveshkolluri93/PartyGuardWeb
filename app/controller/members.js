a31554d4dbbb8e73411619307cb20d242f9d1648
NWMSU\S525084 <S525084@mail.nwmissouri.edu>
2016-11-03 15:13:32 -0500

    Fixed the issue in members.html

--- app/controller/members.js
+++ app/controller/members.js
@@ -10,8 +10,8 @@ function ($scope, $rootScope, $location) {
  $rootScope.isBasic = true;
 
  var members =[
-{"image":"https://s18.postimg.org/zcgklkqbt/1421296_323487294457690_682764613_o_640x401.jpg", "firstName":"Srichaitanya", "lastName":"Peddinti", "email":"peddintisrichaitanya@gmail.com", "phone":"6184391808", "age":"25"},
-{"image":"https://s11.postimg.org/s2n8ksggj/11224756_10203199692231723_7411357595581364253_o.jpg", "firstName":"Lokesh Reddy", "lastName":"Pothapu", "email":"lokeshreddy.p@gmail.com", "phone":"6184391808", "age":"24"},
+{"image":"https://s18.postimg.org/zcgklkqbt/1421296_323487294457690_682764613_o_640x401.jpg", "firstName":"Srichaitanya", "lastName":"Peddinti", "email":"srichaitanya@gmail.com", "phone":"6184391808", "age":"25"},
+{"image":"https://s11.postimg.org/s2n8ksggj/11224756_10203199692231723_7411357595581364253_o.jpg", "firstName":"Lokesh", "lastName":"Pothapu", "email":"lokeshreddy.p@gmail.com", "phone":"6184391808", "age":"24"},
 {"image":"https://s21.postimg.org/jqir5bgxz/10620030_10153379421618265_8349315650679533942_o.jpg", "firstName":"Abishai", "lastName":"Parise", "email":"abishaiparise@gmail.com", "phone":"6184391808", "age":"25"},
 {"image":"https://s15.postimg.org/cyfzd5gx7/13528571_1137806456261626_7331896690535876532_o.jpg", "firstName":"Nihita", "lastName":"Bhimireddy", "email":"nihitabhimireddy@gmail.com", "phone":"6184391808", "age":"24"},
 {"image":"https://s16.postimg.org/4pgvq79n9/14344296_1186119868098540_2947086008923536253_n.jpg", "firstName":"Spandana", "lastName":"Velaga", "email":"spandanavelaga@gmail.com", "phone":"6184391808", "age":"24"},
