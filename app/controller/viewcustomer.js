a031ffbd3bbad3ea27d6426a894499c1995edba0
NWMSU\S525080 <S525080@mail.nwmissouri.edu>
2016-11-03 20:56:15 -0500

    Reolsved issue

--- app/controller/viewcustomer.js
+++ app/controller/viewcustomer.js
@@ -35,7 +35,7 @@ function ($scope, $rootScope, $location, $http) {
 // });
   $http({
         method: 'GET',
-        url: 'http://partyguardservices20161025060016.azurewebsites.net/api/FraternityModels',
+        url: 'https://partyguardservices20161025060016.azurewebsites.net/api/FraternityModels',
         headers: {
         'Authorization': auth
         }}).then(function(result) {
