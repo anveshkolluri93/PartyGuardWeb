359eb9da54fa00cdb64fbf322a620097a65d1591
lokeshreddyp <lokeshreddypothapu@gmail.com>
2016-11-03 23:10:31 -0500

    Create BasicUser.js

---
+++ BasicUser.js
@@ -0,0 +1,8 @@
+
+angular.controller("BasicUserController", ['$scope', '$rootScope', '$location',
+function ($scope, $rootScope, $location ) {
+$rootScope.loginValue = "Logout";
+
+                 $rootScope.isUserLoggedIn = false;
+}
+]);
