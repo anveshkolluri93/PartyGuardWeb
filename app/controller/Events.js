3fb92d212aabe02a1292a0900f261a9ab4ea9494
NWMSU\S525084 <S525084@mail.nwmissouri.edu>
2016-11-15 22:13:15 -0600

    House Profile updated

--- app/controller/Events.js
+++ app/controller/Events.js
@@ -7,10 +7,7 @@ $rootScope.isGuard = true;
 $rootScope.isBasic = true;
 var auth = $rootScope.Author;
 $scope.members =[];
-
-
-                // $location.path('/HostProfile');
- $scope.isUserLoggedIn = true;
+$scope.isUserLoggedIn = true;
 
  $http({
        method: 'POST',
@@ -20,20 +17,10 @@ $scope.members =[];
        }}).then(function(result) {
          $scope.members = result.data;
          console.log($scope.members );
-
-    //   console.log("guards for this fraternity "+JSON.stringify(finalresult));
-
      },function(error) {
      console.log(error);
      });
-//                  var members =[
-//    {"image":"https://s18.postimg.org/zcgklkqbt/1421296_323487294457690_682764613_o_640x401.jpg", "firstName":"Srichaitanya", "lastName":"Peddinti", "email":"peddintisrichaitanya@gmail.com", "phone":"6184391808", "age":"25"},
-//    {"image":"https://s11.postimg.org/s2n8ksggj/11224756_10203199692231723_7411357595581364253_o.jpg", "firstName":"Lokesh Reddy", "lastName":"Pothapu", "email":"lokeshreddy.p@gmail.com", "phone":"6184391808", "age":"24"},
-//   {"image":"https://s21.postimg.org/jqir5bgxz/10620030_10153379421618265_8349315650679533942_o.jpg", "firstName":"Abishai", "lastName":"Parise", "email":"abishaiparise@gmail.com", "phone":"6184391808", "age":"25"},
-//   {"image":"https://s15.postimg.org/cyfzd5gx7/13528571_1137806456261626_7331896690535876532_o.jpg", "firstName":"Nihita", "lastName":"Bhimireddy", "email":"nihitabhimireddy@gmail.com", "phone":"6184391808", "age":"24"},
-//   {"image":"https://s16.postimg.org/4pgvq79n9/14344296_1186119868098540_2947086008923536253_n.jpg", "firstName":"Spandana", "lastName":"Velaga", "email":"spandanavelaga@gmail.com", "phone":"6184391808", "age":"24"},
-//    {"image":"https://s22.postimg.org/jwn4oo6kx/12513993_937898552966797_3549313969414062671_o.jpg","firstName":"Gopi", "lastName":"Cherupally","email":"gopicherupally@gmail.com", "phone":"6184391808", "age":"23"}
-// ];
+
 
 
 
@@ -56,15 +43,12 @@ $scope.members =[];
    }
 
 
-  // $scope.showSuccessAlert = false;
+
 
    $scope.display = function(showSuccessAlertFlag){
     $scope.user['selectedMembers'] = $scope.selected;
     $rootScope.eventCreated = $scope.user;
       $location.path('/EventConfirmation');
-
-  //   $scope.showSuccessAlert = true;
-    // $scope.successTextAlert = "Your Event has been successfully created !!";
    }
 
 
--- app/controller/eventconfirmation.js
+++ app/controller/eventconfirmation.js
@@ -8,11 +8,33 @@ $rootScope.isBasic = true;
 
 $scope.event = $rootScope.eventCreated;
 $scope.assignedGuards = $scope.event.selectedMembers;
+$scope.finalEventDetails = {};
+//var eventDate = $filter('date')($scope.entity.date, 'yyyy-MM-dd');
+//$scope.event['event.eventdate | date: "MM-dd-yyyy"']
+
+console.log("Event details are");
+console.log($scope.event);
+
+$scope.finalEventDetails['eventName'] = $scope.event['eventname']
+$scope.finalEventDetails['eventDateTime']
+
 
 $scope.showSuccessAlert = false;
 
 
 $scope.display = function(showSuccessAlertFlag){
+
+  $http({
+        method: 'POST',
+        url: 'https://partyguardservices20161110094537.azurewebsites.net/api/EventsModels',
+        headers: {
+        'Authorization': auth
+        }}).then(function(result) {
+          $scope.members = result.data;
+          console.log($scope.members );
+      },function(error) {
+      console.log(error);
+      });
 
 $scope.showSuccessAlert = true;
  $scope.successTextAlert = "Your Event has been successfully created !!";
