f3e71046cd8410e7ac8e875de6dd6d3c4953b4bd
NWMSU\S525084 <S525084@mail.nwmissouri.edu>
2016-11-16 00:52:57 -0600

    Integrated Event Creation service

--- app/controller/eventconfirmation.js
+++ app/controller/eventconfirmation.js
@@ -1,22 +1,32 @@
-angular.controller("eventConfirmationController", ['$scope', '$rootScope',
-function ($scope, $rootScope) {
+angular.controller("eventConfirmationController", ['$scope', '$rootScope', '$filter', '$http',
+function ($scope, $rootScope, $filter, $http) {
 $rootScope.loginValue = "Logout";
 $rootScope.isMaster = true;
 $rootScope.isHost = false;
 $rootScope.isGuard = true;
 $rootScope.isBasic = true;
+var auth = $rootScope.Author;
 
 $scope.event = $rootScope.eventCreated;
 $scope.assignedGuards = $scope.event.selectedMembers;
 $scope.finalEventDetails = {};
-//var eventDate = $filter('date')($scope.entity.date, 'yyyy-MM-dd');
-//$scope.event['event.eventdate | date: "MM-dd-yyyy"']
+var eventDate = $filter('date')($scope.event['eventdate'], 'yyyy-MM-dd');
+var eventTime = $filter('date')($scope.event['eventtime'],'HH:mm:ss');
+var eventDateTime = eventDate + 'T' + eventTime + '.0000000+00:00';
+
+
 
 console.log("Event details are");
 console.log($scope.event);
+console.log(eventDate);
+console.log(eventTime);
+console.log(eventDateTime);
 
-$scope.finalEventDetails['eventName'] = $scope.event['eventname']
-$scope.finalEventDetails['eventDateTime']
+$scope.finalEventDetails['eventName'] = $scope.event['eventname'];
+$scope.finalEventDetails['eventDateTime'] = eventDateTime;
+$scope.finalEventDetails['fraternityId'] = $rootScope.fraternityID;
+
+console.log($scope.finalEventDetails);
 
 
 $scope.showSuccessAlert = false;
@@ -24,14 +34,18 @@ $scope.showSuccessAlert = false;
 
 $scope.display = function(showSuccessAlertFlag){
 
+  console.log("inside");
+
   $http({
         method: 'POST',
         url: 'https://partyguardservices20161110094537.azurewebsites.net/api/EventsModels',
+        data: $scope.finalEventDetails,
         headers: {
-        'Authorization': auth
+        'Content-Type': 'application/json'
         }}).then(function(result) {
-          $scope.members = result.data;
-          console.log($scope.members );
+      //    $scope.members = result.data;
+        //  console.log($scope.members );
+        console.log(result);
       },function(error) {
       console.log(error);
       });
