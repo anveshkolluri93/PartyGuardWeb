4c14eea4d8ccd374a6ee67a2ac383f9f097dc103
NWMSU\S525084 <S525084@mail.nwmissouri.edu>
2016-11-15 20:01:44 -0600

    Integrated HouseProfile service

--- app/controller/HouseProfile.js
+++ app/controller/HouseProfile.js
@@ -6,41 +6,20 @@ $rootScope.isHost = false;
 $rootScope.isGuard = true;
 $rootScope.isBasic = true;
 //  $rootScope.master = {};
-var finalresult = [];
+$scope.users =[];
+
 var auth = $rootScope.Author;
 
-console.log("Insisde Hostprofile "+ auth);
-
-// $http({
-//   method: 'GET',
-//   url: 'https://partyguardservices20161110094537.azurewebsites.net/API/Account/UserInfo',
-// }).then(function(result){
-//   finalresult = result.data;
-//   console.log("result before "+JSON.stringify(finalresult));
-// },function(error){
-//   console.log(error);
-// });
 $http({
       method: 'GET',
       url: 'https://partyguardservices20161110094537.azurewebsites.net/API/Account/UserInfo',
       headers: {
       'Authorization': auth
       }}).then(function(result) {
-        finalresult = result.data;
-      console.log("result before "+JSON.stringify(finalresult));
+        $scope.users = result.data;
+      console.log("result before "+JSON.stringify($scope.users));
     },function(error) {
     console.log(error);
     });
- // $scope.master = {};
- // //
- //
- //          $scope.master = angular.copy(users);
-
-
-
-
-// $scope.update = function(user) {
-// $scope.users = user
-// };
 
 }]);
